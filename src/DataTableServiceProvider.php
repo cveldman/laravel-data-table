015a8a7509efe808fd52ed3610c36cea3fd2a694
Corné Veldman <cveldman@hotmail.com>
2022-02-09 20:52:25 +0100

    search on query and eloquent builder

--- src/DataTableServiceProvider.php
+++ src/DataTableServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Veldman\DataTable;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Veldman\DataTable\View\Components\DataTable;
@@ -18,5 +19,75 @@ class DataTableServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'data-table');
 
         Blade::component('data-table', DataTable::class);
+
+        \Illuminate\Database\Eloquent\Builder::macro('search', function (array $columns) {
+            $value = request()->get('search', null);
+
+            if($value != null) {
+                $this->where(function (Builder $query) use ($columns, $value) {
+                    foreach (Arr::wrap($columns) as $column) {
+                        $query->when(
+                            Str::contains($column, '.'),
+
+                            // Relational searches
+                            function (Builder $query) use ($column, $value) {
+                                $parts = explode('.', $column);
+                                $relationColumn = array_pop($parts);
+                                $relationName = join('.', $parts);
+
+                                return $query->orWhereHas(
+                                    $relationName,
+                                    function (Builder $query) use ($relationColumn, $value) {
+                                        $query->where($relationColumn, 'LIKE', "%{$value}%");
+                                    }
+                                );
+                            },
+
+                            // Default searches
+                            function (Builder $query) use ($column, $value) {
+                                return $query->orWhere($column, 'LIKE', "%{$value}%");
+                            }
+                        );
+                    }
+                });
+            }
+
+            return $this;
+        });
+
+        \Illuminate\Database\Query\Builder::macro('search', function (array $columns) {
+            $value = request()->get('search', null);
+
+            if($value != null) {
+                $this->where(function (Builder $query) use ($columns, $value) {
+                    foreach (Arr::wrap($columns) as $column) {
+                        $query->when(
+                            Str::contains($column, '.'),
+
+                            // Relational searches
+                            function (Builder $query) use ($column, $value) {
+                                $parts = explode('.', $column);
+                                $relationColumn = array_pop($parts);
+                                $relationName = join('.', $parts);
+
+                                return $query->orWhereHas(
+                                    $relationName,
+                                    function (Builder $query) use ($relationColumn, $value) {
+                                        $query->where($relationColumn, 'LIKE', "%{$value}%");
+                                    }
+                                );
+                            },
+
+                            // Default searches
+                            function (Builder $query) use ($column, $value) {
+                                return $query->orWhere($column, 'LIKE', "%{$value}%");
+                            }
+                        );
+                    }
+                });
+            }
+
+            return $this;
+        });
     }
 }
